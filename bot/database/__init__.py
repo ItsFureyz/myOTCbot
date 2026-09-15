7af0ad3fef9b735d1304b7351955edb29c3857a1
ItsFureyz <masterfreeze31@gmail.com>
2026-09-15 13:13:40 +0300

    Delete bot directory

--- bot/database/__init__.py
+++
@@ -1,5 +0,0 @@
-from bot.database.db_users import Userx, UserModel
-from bot.database.db_referrals import Referrals, ReferralModel
-from bot.database.db_deals import Deals, DealsModel
-from bot.database.db_workers import Worker, WorkerModel
-from bot.database.db_bans import Banx, BanModel
--- bot/utils/misc/bot_models.py
+++
@@ -1,7 +0,0 @@
-# - *- coding: utf- 8 - *-
-from aiogram.fsm.context import FSMContext
-
-from bot.services.api_session import AsyncRequestSession
-
-FSM = FSMContext
-ARS = AsyncRequestSession
